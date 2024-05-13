b639cc8eb8af52d3c77ce0a6b97d8b792a014c64
Abdelrahman-Maged-Mostafa <podapoda_poda@yahoo.com>
2024-05-13 11:53:11 +0300

    Super Hello

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 // import icon from '../img/icons.svg';//parcel 1
 import icons from 'url:../../img/icons.svg'; //parcel 2
-// import { Fraction } from 'fractional'; //parcel 2
+import { Fraction } from 'fractional'; //parcel 2
 
 class RecipeView extends View {
   parentElement = document.querySelector('.recipe');
